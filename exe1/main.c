ab167e086bc3af5ae18d96ee06813297f695d59f
Gutasss <gutmota23@gmail.com>
2025-04-14 09:45:00 -0300

    exe1-2-3

--- exe1/main.c
+++ exe1/main.c
@@ -27,9 +27,13 @@ void i2c_task(void *p) {
     buf_write[0] = MPUREG_PWR_MGMT_1; // registrador
     buf_write[1] = 1 << 7;            // valor
     i2c_write_blocking(i2c_default, I2C_CHIP_ADDRESS, buf_write, 2, false);
-
+    sleep_ms(100);
+    
     // TODO
     // Configure o acc para operar em 4G
+    buf_write[0] = 0x1C;    
+    buf_write[1] = 1 << 3;   
+    i2c_write_blocking(i2c_default, I2C_CHIP_ADDRESS, buf_write, 2, false);
 
     while (1) {
         vTaskDelay(pdMS_TO_TICKS(200));
--- exe3/main.c
+++ exe3/main.c
@@ -10,7 +10,8 @@
 #include "hardware/i2c.h"
 #include "mpu6050.h"
 
-const int I2C_CHIP_ADDRESS = 0x68;
+const int I2C_MPU_ADDRESS = 0x68;
+const int I2C_BMP280_ADDRESS = 0x76;  // Endereço padrão do BMP280
 const int I2C_SDA_GPIO = 20;
 const int I2C_SCL_GPIO = 21;
 
@@ -21,8 +22,12 @@ void i2c_task(void *p) {
     gpio_pull_up(I2C_SDA_GPIO);
     gpio_pull_up(I2C_SCL_GPIO);
 
-    // TODO
-    // read id chip BMP280
+    uint8_t buffer[1];
+    uint8_t reg_address = 0xD0;  
+
+    // Lê o ID do BMP280
+    i2c_write_blocking(i2c_default, I2C_BMP280_ADDRESS, &reg_address, 1, true);
+    i2c_read_blocking(i2c_default, I2C_BMP280_ADDRESS, buffer, 1, false);
     printf("BMP280 ID: 0x%X \n", buffer[0]);
 
     while (1) {
